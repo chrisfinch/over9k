8b18102d86bf4d882cf0ade16873dfd5d630a764
Chris Finch <chrisfinchy@gmail.com>
2013-03-02 11:49:06 +0000

    Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,12 +15,10 @@ module.exports = function(grunt) {
     },
     exec: {
       commit_build: {
-        cmd: "git add public/js/main-built.js; git commit -m 'commit r.js build file'; git push",
-        stdout: "COMMITTED MAIN-BUILT.JS FROM R.JS BUILD"
+        cmd: "git add public/js/main-built.js; git commit -m 'commit r.js build file'; git push"
       },
       heroku_deploy: {
-        cmd: "git checkout deploy; git merge master; git commit -m 'Merge master>deploy'; git push; git checkout master",
-        stdout: "DEPLOYED TO HEROKU SUCCESSFULLY"
+        cmd: "git checkout deploy; git merge master; git commit -m 'Merge master>deploy'; git push; git checkout master"
       }
     }
   });
