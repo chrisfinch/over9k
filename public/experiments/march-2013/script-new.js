1a37b93e2f6f26ab7ae70cfe12c2503697c2e058
Chris Finch <chrisfinchy@gmail.com>
2013-03-15 11:54:12 +0000

    March 2013 experiement

--- public/experiments/march-2013/script-new.js
+++ public/experiments/march-2013/script-new.js
@@ -1,122 +1,47 @@
 $(function() {
 
+  var width = 999,
+      height = 999,
+      minArea = 1;
 
-var projection = d3.geo.mercator()
-  .scale(14000)
-  .translate([600,3050]);
+  var svg = d3.select("#map").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-var border_color = "#444";
+  var area = d3.select("#area");
 
-var width = 900,
-    height = 900,
-    minArea = 1,
-    formatArea = d3.format(".2r"),
-    formatPercent = d3.format(".2%");
+  var g = svg.append("g");
 
-var x = d3.scale.sqrt()
-    .domain([0, 100])
-    .range([0, width]);
+  var shape = svg.append("path");
 
-var svg = d3.select("#map").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  var projection = d3.geo.mercator()
+    .scale(22000)
+    .translate([700,4550]);
 
-// var bg = svg.append("rect")
-//     .attr("class", "background")
-//     .attr("width", width)
-//     .attr("height", height);
+  var path = d3.geo.path()
+      .projection(null);
 
-var shape = svg.append("path");
+  var simplify = d3.simplify()
+      .topology(true)
+      .area(0)
+      .projection(projection);
 
-// var text = svg.append("text")
-//     .attr("x", width / 2)
-//     .attr("y", height / 2)
-//     .attr("dy", ".35em")
-//     .attr("text-anchor", "middle");
+  d3.json("GBR_adm2.json", function(uk) {
 
-// var path = d3.geo.path()
-//     .projection(function(d) { return [d[0], d[1]]; });
+    var geography = simplify.project(uk);
 
-// var simplify = d3.simplify()
-//     .projection(d3.geo.albers().scale(1300).translate([450, 300]));
+    redraw();
 
-d3.json("GBR_adm2.json", function(data) {
+    area.on("change", function () {
+      minArea = area.node().value;
+      redraw();
+    });
 
-  console.log(data.features);
-
-  for (var i = 0; i < data.features.length; i++) {
-    var feature = data.features[i];
-    var polygons = [];
-
-    //console.log(feature);
-
-    if (feature.geometry.type == "MultiPolygon") {
-      polygons = feature.geometry.coordinates;
-    } else { // Single polygon
-      polygons[0] = feature.geometry.coordinates;
+    function redraw() {
+      shape.attr("d", path(simplify.area(minArea)(geography)));
     }
 
-    for (var j = 0; j < polygons.length; j++) {
-      var polygon = polygons[j];
-      for (var k = 0; k < polygon.length; k++) {
-        var geojson_line = polygon[k];
-        var line, str_line = "M ";
-        for (var l = 0; l < geojson_line.length; l++) {
-          if (l>0)  str_line += " L ";
-          var xy = projection(geojson_line[l]);
-          str_line += xy[0]+" "+xy[1];
-        }
-
-        str_line += " Z";
-        line = svg.append("path").attr("d", str_line);
-        line.attr({stroke:border_color,'stroke-width':1});
-
-      }
-
-    }
-
-  }
-
-  // var m = us.features.reduce(function(m, feature) {
-  //     country = feature.properties.name;
-  //     svg_borders[country]=[];
-  //     var polygons = [];
-  //     if (feature.geometry.type == "MultiPolygon") {
-  //       polygons = feature.geometry.coordinates;
-  //     } else { // Single polygon
-  //       polygons[0] = feature.geometry.coordinates;
-  //     }
-
-  //   console.log("reduce", arguments);
-  //   return m + polygon.reduce(function(m, lineString) {
-  //     return m + lineString.length;
-  //   }, 0);
-  // }, 0);
-
-  // simplify(us);
-  // redraw();
-
-  // bg.on("mousemove", function() {
-  //   minArea = x.invert(d3.mouse(this)[0]);
-  //   redraw();
-  // });
-
-  // function redraw() {
-  //   var n = 0;
-
-  //   shape.attr("d", path({
-  //     type: "MultiPolygon",
-  //     coordinates: us.coordinates.map(function(polygon) {
-  //       return polygon.map(function(lineString) {
-  //         return lineString.filter(function(point) {
-  //           return point[2] >= minArea && ++n;
-  //         });
-  //       });
-  //     })
-  //   }));
-
-  //   text.text(formatArea(minArea) + "px² / " + formatPercent(n / m));
-  // }
-});
+  });
 
 });
+
